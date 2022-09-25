72c160bc9ded3b09f47a9a11c48833fbc72293b0
Soumyaranjan Sahu <sam.2.3.4.5.ranjan@gmail.com>
2022-09-25 18:24:29 +0530

    file structure added

---
+++ src/screens/BootScreens/BootScreen/BootScreen.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
+
+
+const BootScreen = ({  }) => {
+  return( 
+    <View>
+          <Text>
+              
+      </Text>
+    </View>
+  )
+};
+
+
+const styles = StyleSheet.create({});
+
+
+export default BootScreen;
+
