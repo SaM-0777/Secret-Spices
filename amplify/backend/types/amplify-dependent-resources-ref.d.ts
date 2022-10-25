2b13a6fdbe42c257b212b4efb0fe4354e583864f
Soumyaranjan Sahu <sam.2.3.4.5.ranjan@gmail.com>
2022-10-25 15:47:19 +0530

    bottom tab style modified & backend modified

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -12,37 +12,10 @@ export type AmplifyDependentResourcesAttributes = {
             "AppClientID": "string"
         }
     },
-    "function": {
-        "secretspicesdynamodbTrigger9df0f26f": {
-            "Name": "string",
-            "Arn": "string",
-            "Region": "string",
-            "LambdaExecutionRole": "string"
-        },
-        "secretspicesfunction": {
-            "Name": "string",
-            "Arn": "string",
-            "Region": "string",
-            "LambdaExecutionRole": "string"
-        }
-    },
     "storage": {
-        "secretspicesdynamodb": {
-            "Name": "string",
-            "Arn": "string",
-            "StreamArn": "string",
-            "PartitionKeyName": "string",
-            "PartitionKeyType": "string",
-            "SortKeyName": "string",
-            "SortKeyType": "string",
+        "s3secretspicesmediastorage": {
+            "BucketName": "string",
             "Region": "string"
-        }
-    },
-    "api": {
-        "secretspicesrestapi": {
-            "RootUrl": "string",
-            "ApiName": "string",
-            "ApiId": "string"
         }
     }
 }
